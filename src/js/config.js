3601f7c7f70555aaa44326ffcea517c4f2420248
DC\TaiHV <taihv@splus-software.com.vn>
2026-05-06 16:19:36 +0700

    Configure Google Sheets sync

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,4 @@
 window.STUDY_WITH_ME_CONFIG = {
-  googleSheetsWebAppUrl: "",
-  apiToken: ""
+  googleSheetsWebAppUrl: "https://script.google.com/macros/s/AKfycbwFYCroZ_6spLWLSDOdGgMIflcCvarz9OSGEQOlSqFc60G1L_IJlxxohSfZ77JoO14p/exec",
+  apiToken: "cb010175-4ec6-4da6-accc-8dfc5f240c8d"
 };
